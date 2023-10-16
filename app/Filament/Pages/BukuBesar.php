ee5c297ee1e5ffc1e4f79eda05ebe6228cf18f16
aliifam <aliif.arief.m@gmail.com>
2023-10-16 20:47:50 +0700

    intro: filament custom  page is a livewire component

--- app/Filament/Pages/BukuBesar.php
+++ app/Filament/Pages/BukuBesar.php
@@ -9,4 +9,24 @@ class BukuBesar extends Page
     protected static ?string $navigationIcon = 'heroicon-o-book-open';
 
     protected static string $view = 'filament.pages.buku-besar';
+
+    //send custom data to view
+
+    public $data;
+    public $count = 0;
+
+    public function increment()
+    {
+        $this->count++;
+    }
+
+    public function decrement()
+    {
+        $this->count--;
+    }
+
+    public function mount()
+    {
+        $this->data = "makan";
+    }
 }
--- resources/views/filament/pages/buku-besar.blade.php
+++ resources/views/filament/pages/buku-besar.blade.php
@@ -1,3 +1,9 @@
 <x-filament-panels::page>
+    <h1>{{ $data }}</h1>
 
+    <h1>{{ $count }}</h1>
+
+    <button wire:click="increment">+</button>
+
+    <button wire:click="decrement">-</button>
 </x-filament-panels::page>
