ac50f9365eec14f52a38a0c358a527fe79014a88
aliifam <aliif.arief.m@gmail.com>
2023-10-20 16:37:03 +0700

    fix: minor link in landing

--- resources/views/landing.blade.php
+++ resources/views/landing.blade.php
@@ -76,7 +76,7 @@
             <p class="mb-8 text-lg font-normal text-gray-300 lg:text-xl sm:px-16 lg:px-48">Tidak Perlu Khawatir untuk
                 manajemen dan pengelolaan keuangan Pondok Pesantren, dengan SIK PonPes semua jadi Mudah.</p>
             <div class="flex flex-col space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-                <a href="/admin/register"
+                <a href="/dashboard/login"
                     class="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 dark:focus:ring-green-900">
                     Mulai Sekarang
                     <svg class="w-3.5 h-3.5 ml-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
@@ -85,7 +85,7 @@
                             d="M1 5h12m0 0L9 1m4 4L9 9" />
                     </svg>
                 </a>
-                <a href="#"
+                <a href="/#about"
                     class="inline-flex justify-center hover:text-gray-900 items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg border border-white hover:bg-gray-100 focus:ring-4 focus:ring-gray-400">
                     Pelajari Lebih Lanjut
                 </a>
