18515ccf7e294a025e273986b66ba788590c1b93
aliifam <aliif.arief.m@gmail.com>
2023-10-30 20:09:03 +0700

    feat: add laporan aktifitas controller custom

---
+++ app/Http/Controllers/LaporanAktifitasController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class LaporanAktifitasController extends Controller
+{
+    public function exportpdf(Request $request)
+    {
+    }
+
+    public function exportexcel(Request $request)
+    {
+    }
+}
